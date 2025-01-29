872de4a210ecaa8c6d38b90776386573e25168e5
Kevin Lammers <klammers@daytonfreight.com>
2025-01-29 09:13:31 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   apm-begin/src/app/app.config.ts
    	modified:   apm-begin/src/app/products/product-detail/product-detail.component.ts
    	modified:   apm-begin/src/app/products/product-list/product-list.component.ts
    	modified:   apm-begin/src/app/products/product.service.ts
    	modified:   apm-begin/src/app/reviews/review.service.ts

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,10 +1,62 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from './product';
+import { catchError, concatMap, map, Observable, of, tap, throwError } from 'rxjs';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
+  
+  private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
+
+  //get all products
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      tap(() => console.log('In http.get pipline')),
+      catchError(err => this.handleError(err))
+    );
+  }
+
+  //get prodcut by Id 
+  getProduct(id: number): Observable<Product> {
+    const productUrl = this.productsUrl + '/' + id;
+    return this.http.get<Product>(productUrl)
+    .pipe(
+      tap(() => console.log('In http.get by id pipline')),
+      concatMap(product => this.getProductWithReviews(product)),
+      tap(x => console.log(x)),
+      catchError(err=>this.handleError(err))
+    );
+
+  }
+
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews){
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+      .pipe(
+        map(reviews => ({...product, reviews}) as Product )
+      )
+    }
+    else
+      return of(product);
+  }
+
+  //error handler
+  private handleError(err: HttpErrorResponse): Observable<never>{
+    const formattedMessage = this.errorService.formatError(err);
+    return throwError(()=>formattedMessage);
+    // throw formattedMessage;
+  }
 
 }
